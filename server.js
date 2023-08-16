6f5e98e3ab65406b3537c0a7613b52f6a9737aa1
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-08-17 01:43:06 +0300

    ,

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://igor:oj2sGOnjqjxvzdZb@cluster0.q4njwwu.mongodb.net/Contacts_reader?retryWrites=true&w=majority";
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
